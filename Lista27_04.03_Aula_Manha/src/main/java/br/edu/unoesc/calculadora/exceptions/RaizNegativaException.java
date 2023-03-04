df8a5f4fa0367630785a4744dd480669fdcbd2a9
Nathan Wendling <119081872+WendlingNathan@users.noreply.github.com>
2023-03-04 09:46:28 -0300

    Erro na importação

--- Lista27_04.03_Aula_Manha/src/main/java/br/edu/unoesc/calculadora/exceptions/RaizNegativaException.java
+++
@@ -1,8 +0,0 @@
-package br.edu.unoesc.calculadora.exceptions;
-
-@SuppressWarnings("serial")
-public class RaizNegativaException extends RuntimeException {
-	public RaizNegativaException(String msg) {
-		super(msg);
-	}
-}
